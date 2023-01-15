919407920441720d0caf402af61beb1380500a9a
Jon Balon <development@jonbalon.com>
2023-01-15 18:18:44 -0500

    INV-22 Move Index Page Background and Text Colours to Global Styles

--- components/GlobalStyle/index.tsx
+++ components/GlobalStyle/index.tsx
@@ -7,6 +7,9 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
       Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+  
+      background-color: #25273c;
+      color: #e4e5f1;
   }
 
   a {
