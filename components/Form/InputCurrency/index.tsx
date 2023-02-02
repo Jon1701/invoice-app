9c6d4408d5007714bf710ed79d8fc05a07e11b87
Jon Balon <development@jonbalon.com>
2023-02-02 05:36:06 -0500

    INV-124 Input Currency Unified Input

--- components/Form/InputCurrency/index.tsx
+++ components/Form/InputCurrency/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import styled from 'styled-components';
 import { dinero, toDecimal } from 'dinero.js';
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
@@ -10,11 +10,55 @@ import CurrencyFormat, {
 import { CurrencyCode } from '@appTypes/index';
 import getDineroCurrencyInfo from '@utils/currency/getDineroCurrencyInfo';
 
-import {
-  Container,
-  ContainerStyledInputButton,
-  StyledInputButton,
-} from '../common/styles';
+export interface ContainerProps {
+  /**
+   * Indicates if any errors messages are present.
+   */
+  hasErrors?: boolean;
+
+  /**
+   * Indicates if the field is disabled.
+   */
+  disabled?: boolean;
+
+  /**
+   * Indicates if the field is read-only.
+   */
+  readOnly?: boolean;
+}
+
+/**
+ * Component container.
+ */
+export const Container = styled.div<ContainerProps>`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-start;
+  align-items: center;
+
+  padding: 10px;
+  gap: 10px;
+
+  background-color: #484b7c;
+  background-color: ${props => (props.hasErrors ? '#b5445a' : '#484b7c')};
+
+  border-style: ${props =>
+    props.disabled || props.readOnly ? 'dashed' : 'solid'};
+  border-width: 1.5px;
+  border-color: ${props => (props.hasErrors ? '#dc143c' : '#fff')};
+  border-radius: 3px;
+`;
+
+/**
+ * Styled Clear button.
+ */
+const StyledButton = styled.button`
+  all: unset;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 const currencyInfo = {
   [CurrencyCode.CAD]: {
@@ -32,9 +76,7 @@ const currencyInfo = {
 /**
  * Container for the currency code symbol.
  */
-const ContainerSymbol = styled.span`
-  padding: 10px 0 10px 10px;
-`;
+const ContainerSymbol = styled.span``;
 
 /**
  * Styled input.
@@ -42,13 +84,16 @@ const ContainerSymbol = styled.span`
 const StyledInput = styled(CurrencyFormat)`
   all: unset;
   width: 100%;
-  padding: 10px 10px 10px 0;
 
-  &:disabled {
+  &:hover {
+    cursor: text;
+  }
+
+  &:disabled:hover {
     cursor: not-allowed;
   }
 
-  &:read-only {
+  &:read-only:hover {
     cursor: text;
   }
 
@@ -107,6 +152,23 @@ const InputCurrency: React.FC<Props> = ({
   readOnly = false,
   handleClearButtonClick,
 }) => {
+  // Controls visibility of the Clear button.
+  const [showClearButton, setShowClearButton] = useState(false);
+
+  /**
+   * Displays the Clear button when a mouse has entered the div.
+   */
+  const handleMouseEnter = useCallback(() => {
+    return disabled || readOnly ? undefined : setShowClearButton(true);
+  }, []);
+
+  /**
+   * Hides the Clear button when a mouse has left the div.
+   */
+  const handleMouseLeave = useCallback(() => {
+    return disabled || readOnly ? undefined : setShowClearButton(false);
+  }, []);
+
   const hasErrors: boolean =
     errorMessages !== undefined && errorMessages.length > 0;
 
@@ -145,31 +207,44 @@ const InputCurrency: React.FC<Props> = ({
   const { prefix, exponent } = currencyInfo[currencyCode];
 
   return (
-    <Container hasErrors={hasErrors} disabled={disabled} readOnly={readOnly}>
-      <ContainerSymbol>{prefix}</ContainerSymbol>
-      <StyledInput
-        id={id}
-        value={toDecimal(d)}
-        displayType="input"
-        type="text"
-        onValueChange={handleOnValueChange}
-        onBlur={handleOnBlur}
-        fixedDecimalScale={true}
-        thousandSeparator=","
-        decimalScale={exponent}
-        allowNegative={false}
-        disabled={disabled}
-        readOnly={readOnly}
-      />
-      {!disabled && !readOnly ? (
-        <ContainerStyledInputButton>
-          <StyledInputButton
-            type="button"
-            onClick={handleClearButtonClick}
-            disabled={disabled}>
-            <FontAwesomeIcon icon={faCircleXmark} />
-          </StyledInputButton>
-        </ContainerStyledInputButton>
+    <Container
+      hasErrors={hasErrors}
+      disabled={disabled}
+      readOnly={readOnly}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}>
+      <div
+        style={{
+          display: 'flex',
+          flexFlow: 'row nowrap',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          width: '100%',
+        }}>
+        <ContainerSymbol>{prefix}</ContainerSymbol>
+        <StyledInput
+          id={id}
+          value={toDecimal(d)}
+          displayType="input"
+          type="text"
+          onValueChange={handleOnValueChange}
+          onBlur={handleOnBlur}
+          fixedDecimalScale={true}
+          thousandSeparator=","
+          decimalScale={exponent}
+          allowNegative={false}
+          disabled={disabled}
+          readOnly={readOnly}
+        />
+      </div>
+
+      {!disabled &&
+      !readOnly &&
+      handleClearButtonClick !== undefined &&
+      showClearButton ? (
+        <StyledButton type="button" onClick={handleClearButtonClick}>
+          <FontAwesomeIcon icon={faCircleXmark} />
+        </StyledButton>
       ) : undefined}
     </Container>
   );
