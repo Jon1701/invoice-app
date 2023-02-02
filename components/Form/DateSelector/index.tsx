8c1064742bc5a48fca56af7aba996982483bebd1
Jon Balon <development@jonbalon.com>
2023-02-02 05:22:58 -0500

    INV-123 Date Selector Unified Input

--- components/Form/DateSelector/index.tsx
+++ components/Form/DateSelector/index.tsx
@@ -1,17 +1,83 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import DatePicker from 'react-datepicker';
+import styled from 'styled-components';
 import { getUnixTime } from 'date-fns';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 
 import timestampToDate from '@utils/datetime/timestampToDate';
 
-import {
-  Container,
-  ContainerStyledInputButton,
-  StyledInputButton,
-  StyledInput,
-} from '../common/styles';
+export interface ContainerProps {
+  /**
+   * Indicates if any errors messages are present.
+   */
+  hasErrors?: boolean;
+
+  /**
+   * Indicates if the field is disabled.
+   */
+  disabled?: boolean;
+
+  /**
+   * Indicates if the field is read-only.
+   */
+  readOnly?: boolean;
+}
+
+/**
+ * Component container.
+ */
+export const Container = styled.div<ContainerProps>`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-start;
+  align-items: center;
+
+  padding: 10px;
+  gap: 10px;
+
+  background-color: #484b7c;
+  background-color: ${props => (props.hasErrors ? '#b5445a' : '#484b7c')};
+
+  border-style: ${props =>
+    props.disabled || props.readOnly ? 'dashed' : 'solid'};
+  border-width: 1.5px;
+  border-color: ${props => (props.hasErrors ? '#dc143c' : '#fff')};
+  border-radius: 3px;
+`;
+
+/**
+ * Styled input.
+ */
+const StyledInput = styled.input`
+  all: unset;
+  width: 100%;
+
+  &:hover {
+    cursor: text;
+  }
+
+  &:disabled:hover {
+    cursor: not-allowed;
+  }
+
+  &:read-only:hover {
+    cursor: text;
+  }
+
+  box-sizing: border-box;
+`;
+
+/**
+ * Styled Clear button.
+ */
+const StyledButton = styled.button`
+  all: unset;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 export interface DateSelectorProps {
   /**
@@ -68,6 +134,23 @@ const DateSelector: React.FC<DateSelectorProps> = ({
   disabled,
   readOnly,
 }) => {
+  // Controls visibility of the Clear button.
+  const [showClearButton, setShowClearButton] = useState(false);
+
+  /**
+   * Displays the Clear button when a mouse has entered the div.
+   */
+  const handleMouseEnter = useCallback(() => {
+    return disabled || readOnly ? undefined : setShowClearButton(true);
+  }, []);
+
+  /**
+   * Hides the Clear button when a mouse has left the div.
+   */
+  const handleMouseLeave = useCallback(() => {
+    return disabled || readOnly ? undefined : setShowClearButton(false);
+  }, []);
+
   // Checks if any error messages were included.
   const hasErrors: boolean =
     errorMessages !== undefined && errorMessages.length > 0;
@@ -82,7 +165,12 @@ const DateSelector: React.FC<DateSelectorProps> = ({
   };
 
   return (
-    <Container hasErrors={hasErrors} disabled={disabled} readOnly={readOnly}>
+    <Container
+      hasErrors={hasErrors}
+      disabled={disabled}
+      readOnly={readOnly}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}>
       <DatePicker
         selected={timestampToDate(timestamp)}
         onChange={handleChange}
@@ -93,15 +181,13 @@ const DateSelector: React.FC<DateSelectorProps> = ({
         disabled={disabled}
         readOnly={readOnly}
       />
-      {!disabled && !readOnly ? (
-        <ContainerStyledInputButton>
-          <StyledInputButton
-            type="button"
-            onClick={handleClearButtonClick}
-            disabled={disabled}>
-            <FontAwesomeIcon icon={faCircleXmark} />
-          </StyledInputButton>
-        </ContainerStyledInputButton>
+      {!disabled &&
+      !readOnly &&
+      handleClearButtonClick !== undefined &&
+      showClearButton ? (
+        <StyledButton type="button" onClick={handleClearButtonClick}>
+          <FontAwesomeIcon icon={faCircleXmark} />
+        </StyledButton>
       ) : undefined}
     </Container>
   );
